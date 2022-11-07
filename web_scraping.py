d87fe07d2038f990adf21102c7154ef1f7d91872
Megan Cindric <megancindric@gmail.com>
2022-11-07 11:01:57 -0600

    👌 IMPROVE: Scaffolding of scraping of data row function

--- web_scraping.py
+++ web_scraping.py
@@ -1,11 +1,3 @@
-# The purpose of this script is to scrape the Golden Ticket Awards website for award winning coasters
-# From 2010 to 2022.  We will scrape for the following attributes:
-# Ranking
-# Name
-# Park **May need to be cleaned**
-# Supplier
-# Year of Award
-# Year Built
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -19,48 +11,52 @@ page = webdriver.Chrome(service=driver)
 page.get("https://goldenticketawards.com/past-gta-winners/")
 weird_xpaths = {
     2022: {
-        "Wood": ".//h4[contains(.,'Wooden')]/following-sibling::table",
-        "Steel": ".//h4[contains(.,'Steel')]/following-sibling::table"
+        "Wood": ".//h4[contains(.,'Wooden')]/following-sibling::table/tbody",
+        "Steel": ".//h4[contains(.,'Steel')]/following-sibling::table/tbody"
     },
     2021: {
-        "Wood": ".//h3[contains(.,'Wooden')]/following-sibling::figure/table",
-        "Steel": ".//h2[contains(.,'Steel')]/following-sibling::figure/table"
+        "Wood": ".//h3[contains(.,'Wooden')]/following-sibling::figure/table/tbody",
+        "Steel": ".//h2[contains(.,'Steel')]/following-sibling::figure/table/tbody"
     },
     2010: {
-        "Wood": ".//h4[contains(.,'Wood')]/following-sibling::figure/table",
-        "Steel": ".//h4[contains(.,'Steel')]/following-sibling::figure/table"
+        "Wood": ".//h4[contains(.,'Wood')]/following-sibling::figure/table/tbody",
+        "Steel": ".//h4[contains(.,'Steel')]/following-sibling::figure/table/tbody"
     },
 }
-# All links will contain the text "/YYYY-gta-winners/" - we'll use a partial link query
 years = [2022, 2021, 2019, 2018, 2017, 2016,
          2015, 2014, 2013, 2012, 2011, 2010]
+years = [2022]
 for year in years:
-    # This will also allow us to set Year value for each page's entries
-    steel_xpath = ".//h4[contains(.,'Steel')]/following-sibling::figure/table"
-    wood_xpath = ".//h4[contains(.,'Wooden')]/following-sibling::figure/table"
-
+    # Navigating to the correct page, by year
     page.find_element(By.PARTIAL_LINK_TEXT, f"{year}").click()
-    time.sleep(2)
+    # Establishing XPATH to the Wooden & Steel coasters
+    steel_xpath = ".//h4[contains(.,'Steel')]/following-sibling::figure/table/tbody"
+    wood_xpath = ".//h4[contains(.,'Wooden')]/following-sibling::figure/table/tbody"
+    # If the year is one of our exceptions with a unique XPATH, we will update with correct path
     if year in weird_xpaths.keys():
         steel_xpath = weird_xpaths[year]["Steel"]
         wood_xpath = weird_xpaths[year]["Wood"]
-    # 2022 h4/strong /following-sibling::table
-    # 2021 h3/strong /following-sibling::figure/table
-    # 2019 h4/strong /following-sibling::figure/table
-    # 2018 h4/strong /following-sibling::figure/table
-    # 2017 h4 /following-sibling::figure/table
-    # 2016 h4 /following-sibling::figure/table
-    # 2015 h4 /following-sibling::figure/table
-    # 2014 h4 /following-sibling::figure/table
-    # 2013 h4 /following-sibling::figure/table
-    # 2012 h4 /following-sibling::figure/table
-    # 2011 h4 /following-sibling::figure/table
-    # 2010 h4 /following-sibling::figure/table
 
     steel_table = page.find_element(
         By.XPATH, steel_xpath)
+    steel_rows = steel_table.find_elements(By.XPATH, './/tr')
     wood_table = page.find_element(
         By.XPATH, wood_xpath)
-    print(year, steel_table, wood_table)
+    print(steel_rows[0].text, "Year of Award")
+    print(steel_rows[1].text, year)
     page.back()
 page.quit()
+
+
+def process_table_row(table_row, year):
+    pass
+    # We will only extract first 5 values (Rank, Name, Park, Location, Supplier, Year_Built)
+    # We will also append Year_of_Award using year variable
+    # Depending on the year, the structure of the table may be slightly different!
+    # 2010 - Rank, Name, Park, Location, Year_Built, Supplier, Votes
+# 2011 - Rank, Name, Park, Location, Year_Built, Supplier, Points
+# 2012 - Rank, Name, Park, Location, Year_Built, Supplier, Points
+# 2018 - Rank, Name, Park, Location, Supplier, Year_Built, Votes
+# 2019 - Rank, Name, Park, Location, Supplier, Year_Built
+# 2021 - Rank, Name, Park, Location, Supplier, Year_Built
+# 2022 - Rank, Name, Park, Location, Supplier, Year_Built
